f65f294c717e6b27bd7a73437ce1792cbd49799a
dongzhiwei <2660847388@qq.com>
2021-10-28 16:43:38 +0800

    sql批量读取数据

--- internal/services/store_to_center.go
+++ internal/services/store_to_center.go
@@ -7,16 +7,25 @@ import (
 )
 
 type Store struct {
-
 }
 
 func (s *Store) GetStoreInfo() (store *models.StoreInfo, err error) {
 	storeInfo := new(models.StoreInfo)
 	err = dao.Orm.Find(storeInfo).Error
 	//info, err := json.Marshal(storeInfo)
-	if err != nil{
+	if err != nil {
 		log.Println("[services.GetStoreInfo], err")
 	}
 
 	return storeInfo, err
+}
+
+// 批量存数据库
+func (s *Store) CreateStoreInfo(allStore []models.StoreInfo) (err error) {
+	for i := 0; i < len(allStore) - 1; i++ {
+		err = dao.Orm.Create(allStore[i+1]).Error
+	}
+
+
+	return err
 }
---
+++ sql.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"inherited/internal"
+	"inherited/internal/models"
+	"inherited/internal/services"
+	"log"
+	"strconv"
+)
+
+func main() {
+	if err := internal.Init(); err != nil {
+		log.Println("Init failed." + err.Error())
+		return
+	}
+	f, err := excelize.OpenFile("data.xlsx")
+	if err != nil {
+		println(err.Error())
+		fmt.Println("er")
+		return
+	}
+	data := make([]models.StoreInfo, 4)
+	// 获取工作表中指定单元格的值
+	//cell, err := f.GetCellValue("txt001", "B2")
+	//if err != nil {
+	//	println(err.Error())
+	//	return
+	//}
+	//println(cell)
+
+	//获取 Sheet1 上所有单元格
+	rows, err := f.GetRows("总仓到配送中心")
+	for i, row := range rows {
+		fmt.Println("i", i)
+		data[i].Id = i + 1
+		data[i].Number = row[0]
+		data[i].UserName = row[1]
+		data[i].ProductName = row[2]
+		NeedNum, _ := strconv.Atoi(row[3])
+		data[i].NeedSum = NeedNum
+		data[i].ProductType = row[4]
+	}
+	println()
+	store := new(services.Store)
+	err = store.CreateStoreInfo(data)
+	if err != nil {
+		fmt.Println(err, 123)
+	}
+	fmt.Println(data)
+	//计时器
+	//t1 := time.Now()
+	//rows, _ := f.GetRows("txt001")
+	//for err, _ := range rows {
+	//	//fmt.Println(err)
+	//	if err == 0 {
+	//		fmt.Printf("2")
+	//	}
+	//	//println(row[0], row[1], row[2], row[3], row[4])
+	//	//println(row[0], row[1], row[2], row[3], row[4])
+	//	//println(row[0], row[1], row[2], row[3], row[4])
+	//	//println(row[0], row[1], row[2], row[3], row[4])
+	//	//println(row[0], row[1], row[2], row[3], row[4])
+	//
+	//}
+	//elapsed := time.Since(t1)
+	//fmt.Println("App elapsed: ", elapsed)
+
+	//并发读
+	//t1 := time.Now()
+	//wg := sync.WaitGroup{}
+	//wg.Add(16010)
+	//count := 0
+	//rows, _ := f.GetRows("txt001")
+	//for _, row := range rows {
+	//	go func(i []string) {
+	//		if i == nil {
+	//			fmt.Printf("2")
+	//		}
+	//		wg.Done()
+	//	}(row)
+	//
+	//}
+	//wg.Wait()
+	//elapsed := time.Since(t1)
+	//fmt.Println("App elapsed: ", elapsed, "count=", count)
+
+	//ch := make(chan int, 10)
+
+	//ch <- 1
+	//<-ch
+	//fmt.Println(1)
+
+}
